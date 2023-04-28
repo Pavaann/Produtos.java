75867cc9f3e0d10f8c70cf95d6d59f3610488227
Pavaann <pavanrafaela18@gmail.com>
2023-04-27 21:49:53 -0300

    second commit

--- src/program/java/Product.java
+++ src/program/java/Product.java
@@ -1,4 +1,18 @@
 package program.java;
 
 public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+    public void addProduts(int quantity){
+        this.quantity += quantity;
+    }
+    public void removeProduts(int quantity){
+        this.quantity -= quantity;
+    }
 }
+
